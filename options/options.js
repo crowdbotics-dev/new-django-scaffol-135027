887e31bff8ca2cee21b1233d9ea4e05456fa58c6
Crowdbotics <engineering@crowdbotics.com>
2024-04-24 18:05:42 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "new-django-scaffol-135027",
+  url: "https://new-django-scaffol-135027.botics.co",
+  api: "https://new-django-scaffol-135027.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
